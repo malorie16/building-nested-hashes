b3c65e540d60a4f302214485f85313cd42da1984
Robin Example <malorie.casimir@flatironschool.com>
2019-09-18 20:14:08 +0000

    automatically backed up by learn

--- spec/01_challenge_spec.rb
+++ spec/01_challenge_spec.rb
@@ -2,8 +2,8 @@ require_relative 'spec_helper'
 require_relative '../lib/first_challenge.rb'
 
 describe "#first_challenge" do 
-	it "sets a variable, epic_tragedy, equal to hash with keys of family names and values of empty hashes" do 
-    expect(first_challenge.keys).to eq([:montague, :capulet])
-    expect(first_challenge.values).to eq([{}, {}])
-  end
+# 	it "sets a variable, epic_tragedy, equal to hash with keys of family names and values of empty hashes" do 
+#     expect(first_challenge.keys).to eq([:montague, :capulet])
+#     expect(first_challenge.values).to eq([{}, {}])
+#   end
 end
